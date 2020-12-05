6a982a6a3530f68aeaa116a3938d4347a406b85d
Gabriel Reis <gabrielcnr@gmail.com>
2020-12-05 20:28:06 +0000

    spacesssss

--- day05/day05.py
+++ day05/day05.py
@@ -1,39 +1,39 @@
 def get_seat_id(encoded_seat):
-	row, col = decode_seat(encoded_seat)
-	return row * 8 + col
+    row, col = decode_seat(encoded_seat)
+    return row * 8 + col
 
 
 def decode_seat(encoded_seat):
-	s = encoded_seat.translate(
+    s = encoded_seat.translate(
         {ord("F"): "0", ord("B"): "1", ord("L"): "0", ord("R"): "1"})
-	return int(s[:7], 2), int(s[7:], 2)
+    return int(s[:7], 2), int(s[7:], 2)
 
 
 def part1():
-	return max(get_seat_id(encoded_seat)
-		for encoded_seat in open("day05_input.txt"))
+    return max(get_seat_id(encoded_seat)
+        for encoded_seat in open("day05_input.txt"))
 
 def part2():
-	seats = sorted(get_seat_id(s) for s in open("day05_input.txt"))
-	min_seat, max_seat = seats[0], seats[-1]
-	my_seat, = set(range(min_seat, max_seat + 1)) - set(seats)
-	return my_seat
+    seats = sorted(get_seat_id(s) for s in open("day05_input.txt"))
+    min_seat, max_seat = seats[0], seats[-1]
+    my_seat, = set(range(min_seat, max_seat + 1)) - set(seats)
+    return my_seat
 
 
 def test_part1():
-	assert (44, 5) == decode_seat("FBFBBFFRLR")
-	assert 357 == get_seat_id("FBFBBFFRLR")
+    assert (44, 5) == decode_seat("FBFBBFFRLR")
+    assert 357 == get_seat_id("FBFBBFFRLR")
 
-	assert (70, 7) == decode_seat("BFFFBBFRRR")
-	assert 567 == get_seat_id("BFFFBBFRRR")
+    assert (70, 7) == decode_seat("BFFFBBFRRR")
+    assert 567 == get_seat_id("BFFFBBFRRR")
 
-	assert (14, 7) == decode_seat("FFFBBBFRRR")
-	assert 119 == get_seat_id("FFFBBBFRRR")
+    assert (14, 7) == decode_seat("FFFBBBFRRR")
+    assert 119 == get_seat_id("FFFBBBFRRR")
 
-	assert (102, 4) == decode_seat("BBFFBBFRLL")
-	assert 820 == get_seat_id("BBFFBBFRLL")
+    assert (102, 4) == decode_seat("BBFFBBFRLL")
+    assert 820 == get_seat_id("BBFFBBFRLL")
 
 
 if __name__ == "__main__":
-	print(f"Max seat ID: {part1()}")
-	print(f"My seat is: {part2()}")
+    print(f"Max seat ID: {part1()}")
+    print(f"My seat is: {part2()}")
