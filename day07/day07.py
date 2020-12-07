15764dcb17c62498e339fbb822935a73a2c5efe6
Gabriel Reis <gabrielcnr@gmail.com>
2020-12-07 11:55:13 +0000

    Day 7 - part 2 complete

--- day07/day07.py
+++ day07/day07.py
@@ -65,9 +65,39 @@ def test_part1():
     assert {'bright white', 'muted yellow', 'dark orange', 'light red'} == visited
 
 
+TEST_INPUT_2 = """\
+shiny gold bags contain 2 dark red bags.
+dark red bags contain 2 dark orange bags.
+dark orange bags contain 2 dark yellow bags.
+dark yellow bags contain 2 dark green bags.
+dark green bags contain 2 dark blue bags.
+dark blue bags contain 2 dark violet bags.
+dark violet bags contain no other bags."""
+
+
+def part2(input_, lookup_bag):
+    rules, reversed_rules_lookup = populate_lookup_tables(input_)
+    bags = []
+    queue = deque([lookup_bag])
+    while queue:
+        bag = queue.popleft()
+        bags.append(bag)
+        for child, count in rules[bag].items():
+            queue.extend([child] * count)
+    return bags
+
+
+def test_part2():
+    # we include the target bag in the list of bags - so we need to discount 1
+    assert 126 == len(part2(TEST_INPUT_2, "shiny gold")) - 1
+
+
 if __name__ == "__main__":
     input_ = open("day07_input.txt").read()
     bags_containing_shiny_gold = part1(input_, "shiny gold")
     count = len(bags_containing_shiny_gold)
     print(f"Part 1 - bags eventually containing shiny gold: {count}")
 
+    nested_bags = part2(input_, "shiny gold")
+    count = len(nested_bags)
+    print(f"Part 2 - total nested bags: {count-1}")
