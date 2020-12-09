8b465c09930515b4502f6f67f83c6fa163ade370
Gabriel Reis <gabrielcnr@gmail.com>
2020-12-09 09:32:49 +0000

    Day 9 - Part 2 - complete

--- day09/day09.py
+++ day09/day09.py
@@ -76,6 +76,37 @@ def test_part1():
     assert 127 == part1(5, TEST_INPUT_1)
 
 
+def part2(cb_size, input_):
+    invalid_num = part1(cb_size, input_)
+
+    numbers = (int(n) for n in input_.splitlines())
+
+    contiguous = []
+    contiguous_sum = 0
+    while contiguous_sum != invalid_num:
+        if contiguous_sum < invalid_num:
+            n = next(numbers)
+            contiguous.append(n)
+            contiguous_sum += n
+        elif contiguous_sum > invalid_num:
+            n = contiguous.pop(0)
+            contiguous_sum -= n
+
+    min_ = min(contiguous)
+    max_ = max(contiguous)
+    return contiguous, contiguous_sum, min_, max_
+
+def test_part2():
+    contiguous, contiguous_sum, min_, max_ = part2(5, TEST_INPUT_1)
+    assert [15, 25, 47, 40] == contiguous
+    assert 127 == contiguous_sum
+    assert 15 == min_
+    assert 47 == max_
+
+
 if __name__ == "__main__":
     input_ = open("day09_input.txt").read()
     print("Part 1:", part1(25, input_))
+
+    contiguous, contiguous_sum, min_, max_ = part2(25, input_)
+    print(f"Part 2: {min_=}, {max_=}, {min_+max_=}")
