9ec7e5e43aee34908e57b954be9ca7a1e76eabcd
Gabriel Reis <gabrielcnr@gmail.com>
2020-12-19 18:57:23 +0000

    Day 18 - Part 2 complete

--- day18/day18.py
+++ day18/day18.py
@@ -58,6 +58,42 @@ def part1(input_: str):
     return sum(calc(expr) for expr in input_.splitlines())
 
 
+def calc2(expr):
+    if isinstance(expr, int):
+        return expr
+    stack = list(iter_expressions(expr))
+    while len(stack) > 1:
+        # look for addition first
+        if "+" in stack:
+            add_idx = stack.index("+")
+            left, op, right = stack[add_idx - 1: add_idx + 2]
+            stack_head = stack[:add_idx - 1]
+            stack_tail = stack[add_idx + 2:]
+        else:
+            stack_head = []
+            left, op, right, *stack_tail = stack
+        a = calc2(left)
+        b = calc2(right)
+        stack = stack_head + [eval(f"a {op} b", {"a": a, "b": b})] + stack_tail
+    result, = stack
+    return int(result)
+
+
+def test_part2_calc():
+    assert 231 == calc2("1 + 2 * 3 + 4 * 5 + 6")
+    assert 51 == calc2("1 + (2 * 3) + (4 * (5 + 6))")
+    assert 46 == calc2("2 * 3 + (4 * 5)")
+    assert 1445 == calc2("5 + (8 * 3 + 9 + 3 * 4 * 3)")
+    assert 669060 == calc2("5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))")
+    assert 23340 == calc2("((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2")
+
+
+def part2(input_: str):
+    return sum(calc2(expr) for expr in input_.splitlines())
+
+
 if __name__ == '__main__':
     input_ = open("input18.txt").read()
     print("Part 1:", part1(input_))
+
+    print("Part 2:", part2(input_))
